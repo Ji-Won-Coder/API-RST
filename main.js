991bab32654d2dfe816695ee147b073704051602
Mary Martinez <maria.martinez580@misena.edu.co>
2023-02-16 13:14:46 -0500

    la alegria por el caño

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 console.log('jajajjaa FUNCIONO !!!')
 
-const url='https://cataas.com/api/cats?tags=cute';
+const url='https://cataas.com/cat/says/hello%20world!';
 
 fetch(url) //devuelve una promesa y la resuelvo con un metodo then
 .then(res => res.json())
 .then(data =>{
     const img =document.querySelector('img')
-    img.src = data[0].url;
-});
+    img.src = data[0].url
+})
